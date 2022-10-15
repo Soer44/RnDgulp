641c588accdc47d637100361f7e2ed5bd19d9cd1
ESB <volga_egor@mail.ru>
2022-10-15 23:01:24 +0300

    третья попытка собрать

--- src/js/app.js
+++ src/js/app.js
@@ -5,3 +5,7 @@ flsFunctions.isWebp();
 import Swiper, { Navigation, Pagination } from 'swiper'; // подключение слайдера свайпер
 
 const swiper = new Swiper();
+
+document.querySelector(".check").addEventListener("change", function() {
+	document.querySelector(".btn").disabled = !document.querySelector(".check").checked;
+  });
